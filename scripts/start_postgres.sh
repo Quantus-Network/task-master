0c6c6f63b6e021b3363244327afafe910cb29ce1
NIK <nheger@gmail.com>
2025-10-15 13:37:48 +0800

    Revert "better start script"
    This reverts commit 59ef96d091227fb825734d07235cab9770857abc.

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,16 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-# Ensure Podman machine exists
-if ! podman machine list | grep -q running; then
-  if ! podman machine list | grep -q "podman-machine-default"; then
-    echo "Creating Podman machine..."
-    podman machine init
-  fi
-  echo "Starting Podman machine..."
-  podman machine start
-fi
-
-# Run Postgres service via compose
-echo "Starting Postgres container..."
 podman compose -f docker-compose.yaml up -d postgres
