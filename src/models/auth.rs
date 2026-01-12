e4d723e9a22d86cd6acda572911e146f08d14b37
Beast <idewagedebisma@gmail.com>
2026-01-12 15:01:35 +0800

    fix: test error because of unit struct

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -8,7 +8,7 @@ pub struct TokenClaims {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct RequestChallengeBody;
+pub struct RequestChallengeBody {}
 
 #[derive(Debug, Serialize)]
 pub struct RequestChallengeResponse {
